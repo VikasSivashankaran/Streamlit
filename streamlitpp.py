e26e32eaa3fac21f4f45da107c8b8abab0b48b5d
VikasSivashankaran <vikasbmca@gmail.com>
2024-09-20 19:02:51 +0530

    new

--- streamlitpp.py
+++ streamlitpp.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import plotly.express as px
 import streamlit as st
-
+st.balloons()
 url = 'laptops_data.csv'
 data = pd.read_csv(url)
 
@@ -77,4 +77,4 @@ st.subheader('Pie Chart of Rating Distribution by Device Name')
 fig11 = px.pie(data, values="Rating", names="Device Name", color_discrete_sequence=px.colors.sequential.RdBu, opacity=0.7, hole=0.5)
 st.plotly_chart(fig11)
 
-st.balloons()
+
